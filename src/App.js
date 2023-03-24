d0114b71440ec4e31da1284d27c8727e421f9937
feherszabolcs <feherszabi2005@gmail.com>
2023-03-24 20:19:32 +0100

    cursor effect

--- src/App.js
+++ src/App.js
@@ -4,6 +4,21 @@ import { Navbar, Footer, Welcome, About } from './components';
 import { particlesCursor } from 'threejs-toys'
 const App = () => {
 
+  particlesCursor({
+    el: document.getElementById('app'),
+    gpgpuSize: 256,
+    colors: [0x00ff00, 0x0000ff],
+    color: 0xff0000,
+    coordScale: 1.5,
+    noiseIntensity: 0.001,
+    noiseTimeCoef: 0.0001,
+    pointSize: 5,
+    pointDecay: 0.005,
+    sleepRadiusX: 250,
+    sleepRadiusY: 250,
+    sleepTimeCoefX: 0.001,
+    sleepTimeCoefY: 0.002
+  })
 
   return (
       <div className='container' id='app'>
