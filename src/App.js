9885be7ebfc69040160f8e6facfbae441d819b60
feherszabi@sulid.hu <feherszabi@sulid.hu>
2023-03-25 09:21:12 +0100

    stable

--- src/App.js
+++ src/App.js
@@ -4,21 +4,6 @@ import { Navbar, Footer, Welcome, About } from './components';
 import { particlesCursor } from 'threejs-toys'
 const App = () => {
 
-  particlesCursor({
-    el: document.getElementById('app'),
-    gpgpuSize: 256,
-    colors: [0x00ff00, 0x0000ff],
-    color: 0xff0000,
-    coordScale: 1.5,
-    noiseIntensity: 0.001,
-    noiseTimeCoef: 0.0001,
-    pointSize: 5,
-    pointDecay: 0.005,
-    sleepRadiusX: 250,
-    sleepRadiusY: 250,
-    sleepTimeCoefX: 0.001,
-    sleepTimeCoefY: 0.002
-  })
 
   return (
       <div className='container' id='app'>
@@ -37,8 +22,24 @@ const App = () => {
           <Footer />
         </section>
       </div>
-
   )
 }
+
+
+particlesCursor({
+  el: document.getElementById('app'),
+  gpgpuSize: 256,
+  colors: [0x00ff00, 0x0000ff],
+  color: 0xff0000,
+  coordScale: 1.5,
+  noiseIntensity: 0.001,
+  noiseTimeCoef: 0.0001,
+  pointSize: 5,
+  pointDecay: 0.005,
+  sleepRadiusX: 250,
+  sleepRadiusY: 250,
+  sleepTimeCoefX: 0.001,
+  sleepTimeCoefY: 0.002
+})
 
 export default App;
