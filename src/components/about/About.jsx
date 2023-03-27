603ca87abcc1f17d98fcff71bca7ee8fae9a70f0
feherszabolcs <feherszabi2005@gmail.com>
2023-03-27 18:43:57 +0200

    started about tab

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import './about.css'
 
-const About = () =>{
-    return(
-        <div>
-            about
+const About = () => {
+    return (
+        <div className="about-container">
+            <div className="about-head">
+                <h1>Rólam</h1>
+                <img src='' alt="feherszabolcs" />
+            </div>
+            <div className="skills"> 
+
+            </div>
         </div>
     )
 }
